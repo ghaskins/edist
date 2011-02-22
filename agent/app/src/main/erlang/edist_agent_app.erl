c974fe4dae8d429afd2a69ecbed493bdb01fef05
Gregory Haskins <ghaskins@novell.com>
2011-02-22 13:31:14 -0500

    Add "app" and "path" args
    Signed-off-by: Gregory Haskins <ghaskins@novell.com>

--- agent/app/src/main/erlang/edist_agent_app.erl
+++ agent/app/src/main/erlang/edist_agent_app.erl
@@ -4,11 +4,25 @@
 
 -export([start/2, stop/1]).
 
-start(_Type, _StartArgs) ->
-    edist_agent_sup:start_link().
+start(_Type, StartArgs) ->
+    RequiredArgs = [app, path],
+
+    lists:foldl(fun(Arg, _Acc) ->
+			case proplists:get_value(Arg, StartArgs) of
+			    undefined -> throw({"Missing required arg", Arg});
+			    _ -> ok
+			end
+		end,
+		void, RequiredArgs),
+
+    App = proplists:get_value(app, StartArgs),
+    Path = proplists:get_value(path, StartArgs),
+
+    edist_agent_sup:start_link(App, Path).
 
 stop(_State) ->
     ok.
+
 
 
 
--- agent/app/src/main/erlang/edist_agent_sup.erl
+++ agent/app/src/main/erlang/edist_agent_sup.erl
@@ -4,7 +4,7 @@
 -module(edist_agent_sup).
 -behaviour(supervisor).
 
--export([start_link/0, start_child/1]).
+-export([start_link/1, start_child/1]).
 
 %% --------------------------------------------------------------------
 %% Internal exports
@@ -15,13 +15,13 @@
 
 -define(SERVER, ?MODULE).
 
-start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+start_link(Args) ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, Args).
 
 start_child(ChildSpec) ->
     supervisor:start_child(?MODULE, ChildSpec).
 
-init([]) ->
+init([_App, _Path]) ->
     {ok,{{one_for_all,0,1},
 	 [{'edist-agent',
 	   {edist_agent,start_link,[]},
