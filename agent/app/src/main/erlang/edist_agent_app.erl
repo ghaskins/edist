5f46c76c502e9a97af9d23ec973c6986fb8ab41a
Gregory Haskins <ghaskins@novell.com>
2011-02-22 11:00:11 -0500

    Let the agent-link library perform the contact initialization
    Signed-off-by: Gregory Haskins <ghaskins@novell.com>

--- agent/app/src/main/erlang/edist_agent_app.erl
+++ agent/app/src/main/erlang/edist_agent_app.erl
@@ -5,16 +5,7 @@
 -export([start/2, stop/1]).
 
 start(_Type, _StartArgs) ->
-    % FIXME
-    StartArgs = [{contacts, ["contact1@linux-mp", "contact2@linux-mp"]}],
-
-    Contacts = [ list_to_atom(Contact) ||
-		   Contact <- proplists:get_value(contacts, StartArgs, [])
-	       ],
-
-    error_logger:info_msg("Starting with contacts: ~p (~p)~n",
-			  [Contacts, StartArgs]),
-    edist_agent_sup:start_link(Contacts).
+    edist_agent_sup:start_link().
 
 stop(_State) ->
     ok.
--- agent/app/src/main/erlang/edist_agent_sup.erl
+++ agent/app/src/main/erlang/edist_agent_sup.erl
@@ -4,7 +4,7 @@
 -module(edist_agent_sup).
 -behaviour(supervisor).
 
--export([start_link/1, start_child/1]).
+-export([start_link/0, start_child/1]).
 
 %% --------------------------------------------------------------------
 %% Internal exports
@@ -15,28 +15,18 @@
 
 -define(SERVER, ?MODULE).
 
-start_link(Contacts) ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, [Contacts]).
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 start_child(ChildSpec) ->
     supervisor:start_child(?MODULE, ChildSpec).
 
-init([Contacts]) ->
-
-    F = fun(Contact) ->
-		{Contact,
-		 {contact_fsm, start_link, [Contact]},
-		 transient,
-		 brutal_kill,
-		 worker,
-		 [contact_fsm]}
-	end,
-    
+init([]) ->
     {ok,{{one_for_all,0,1},
 	 [{'edist-agent',
 	   {edist_agent,start_link,[]},
 	   permanent, 2000, worker,[edist_agent]}
-	 ] ++ [ F(Contact) || Contact <- Contacts ]
+	 ]
 	}
     }.
 
