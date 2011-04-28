0bddd0da9a534063077486faf479aa4b9103d6e9
Gregory Haskins <ghaskins@novell.com>
2011-04-28 11:08:23 -0400

    Cleanup test
    Signed-off-by: Gregory Haskins <ghaskins@novell.com>

--- controller/app/src/test/erlang/release_handler_tests.erl
+++ controller/app/src/test/erlang/release_handler_tests.erl
@@ -2,38 +2,41 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+initialize_test() ->
+    application:stop(gproc),
+    application:set_env(gproc, gproc_dist, 'all'),
+    application:start(gproc).
+
 handler_test() ->
     Rel = "test",
     Vsn1 = "1.1",
     Vsn2 = "1.2",
     ClientName = "testclient",
 
-    application:stop(gproc),
-    application:set_env(gproc, gproc_dist, 'all'),
-    application:start(gproc),
-
     {ok, Pid} = edist_release_handler:start_link(Rel),
 
     gproc:reg({p, g, {edist_release_subscriber, Rel}}, ClientName),
     edist_event_bus:notify({release, Rel}, {commit, Vsn1}),
 
-    wait({update_available, Vsn1}),
-    notify({loaded, Vsn1}, ClientName),
-    
+    handle_update(Vsn1, ClientName),
+
     % cause an overlapping update
     edist_event_bus:notify({release, Rel}, {commit, Vsn2}),
     
-    wait({reload, Vsn1}),
-    notify({online, Vsn1}, ClientName),
+    handle_reload(Vsn1, ClientName),
     
-    wait({update_available, Vsn2}),
-    notify({loaded, Vsn2}, ClientName),
-    
-    wait({reload, Vsn2}),
-    notify({online, Vsn2}, ClientName),
+    handle_update(Vsn2, ClientName),
+    handle_reload(Vsn2, ClientName),
     
     ok.
 
+handle_update(Vsn, ClientName) ->
+    wait({update_available, Vsn}),
+    notify({loaded, Vsn}, ClientName).
+
+handle_reload(Vsn, ClientName) ->    
+    wait({reload, Vsn}),
+    notify({online, Vsn}, ClientName).    
 
 wait(Event) ->
     receive
